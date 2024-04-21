70b3efab0e983538a632297bc864ebf91b080d39
Yassin-Ramadan <y.ramadan1118@gmail.com>
2024-04-21 20:40:28 +0200

    Updated the bookPage html, css and js files

--- javascript/BookPage.js
+++ javascript/BookPage.js
@@ -1,4 +1,6 @@
 
+var accountType = JSON.parse(localStorage.getItem('userInfo')).account_type;
+var loggedIn = JSON.parse(localStorage.getItem('loggedIn'));
 function getUrlParameter(name) {
     const params = new URLSearchParams(window.location.search);
     console.log(params.get(name));
@@ -10,6 +12,10 @@ document.addEventListener("DOMContentLoaded", function(){
     var storedBooks = localStorage.getItem('libraryBooks');
     var id = getUrlParameter('id');
     document.getElementById("Btns").innerHTML = btns(id);
+    if(accountType == "User" || !loggedIn){
+        document.getElementById("del").style.display= "none";
+        document.getElementById("edit").style.display= "none";
+    }
     var books= [];
     if (storedBooks) {
         books = JSON.parse(storedBooks);
@@ -17,8 +23,10 @@ document.addEventListener("DOMContentLoaded", function(){
     books.forEach(function (book) {
         if(book.bookID==id){
             container.innerHTML += bookdefinition(book);
-            getElementById("del").addEventListener("click", function(){
+            document.getElementById("del").addEventListener("click", function(){
                 book.numberofcopies = 0;
+                localStorage.setItem('libraryBooks', JSON.stringify(books));
+                window.location.href = "ViewBooks.html";
             });
         }
     });
@@ -29,7 +37,7 @@ function btns(id){
     return `
             <div>
                 <button class="borrow"><a class="btnword" href="BorrowBook.html" target="_blanc">Borrow</a></button>
-                <button class="edit"><a class="btnword" href="EditBook.html?id=${id}" target="_blanc">Edit</a></button>
+                <button class="edit" id="edit"><a class="btnword" href="EditBook.html?id=${id}" target="_blanc">Edit</a></button>
                 <input class="delete" id="del" type="button" value="Delete">
             </div>
             <div>
