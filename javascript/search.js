2cc0a5609e939d37a4bf5fa715526d8cdcc7cec5
AhmedElsayed2004 <ahmedelsayed12004@gmail.com>
2024-04-21 19:55:28 +0200

    show error message if there is no books and change effect of hover on the book

--- javascript/search.js
+++ javascript/search.js
@@ -11,18 +11,33 @@ document.addEventListener("DOMContentLoaded", function() {
     var books = getBooksData();
     var container = document.getElementById("all_books");
 
+    var isrender=false;
     
     books.forEach(function (book) {
-        if('title' == filterValue && book.title.toLowerCase().includes(searchValue.toLowerCase())){
-            container.innerHTML+= generateBookHTML(book);
-        }
-        else if('author' == filterValue && book.author.toLowerCase().includes(searchValue.toLowerCase())){
-            container.innerHTML+= generateBookHTML(book);
-        }
-        else if('category' == filterValue && book.category.toLowerCase().includes(searchValue.toLowerCase())){
-            container.innerHTML+= generateBookHTML(book);
+        if(book.numberofcopies>0) {
+            if('title' == filterValue && book.title.toLowerCase().includes(searchValue.toLowerCase())){
+                container.innerHTML+= generateBookHTML(book);
+                isrender=true;
+            }
+            else if('author' == filterValue && book.author.toLowerCase().includes(searchValue.toLowerCase())){
+                container.innerHTML+= generateBookHTML(book);
+                isrender=true;
+
+            }
+            else if('category' == filterValue && book.category.toLowerCase().includes(searchValue.toLowerCase())){
+                container.innerHTML+= generateBookHTML(book);
+                isrender=true;
+
+            }
         }
     });
+
+    if(!isrender){
+        container.innerHTML+=
+        `
+            <p style = "font-size:20px">there is no books</p>
+        `
+    }
 
 });
 
@@ -38,15 +53,16 @@ function getBooksData(){
 
 function generateBookHTML(book) {
     return `
-        <div class="book_block">
-            <a href="BookPage.html?id=${book.bookID}" class="book_link">
-                <img src="${book.image}" alt="Book Cover" class="book_image">
-                <div class="book_details">
-                    <h2 class="book_title">${book.title}</h2>
-                    <p class="book_author">${book.author}</p>
-                    <p class="book_price">${book.price}$</p>
-                </div>
-            </a>
-        </div>
+    <div class="book_block">
+        <a href="BookPage.html?id=${book.bookID}" class="book_link">
+            <img src="${book.image}" alt="Book Cover" class="book_image">
+            <div class="book_details">
+                <h2 class="book_title">Title : ${book.title}</h2>
+                <p class="book_author">Author : ${book.author}</p>
+                <p class="book_category">Category : ${book.category}</p>
+                <p class="book_price">Price : ${book.price}$</p>
+            </div>
+        </a>
+    </div>
     `;
 }
