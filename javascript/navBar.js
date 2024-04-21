67af1be1245da4c3debee12365402bc223c3af92
Omar_Abdelmonem <147417133+Omar-Mohamed0@users.noreply.github.com>
2024-04-21 22:22:02 +0200

    Update navBar.js

--- javascript/navBar.js
+++ javascript/navBar.js
@@ -1,3 +1,46 @@
+document.addEventListener("DOMContentLoaded",function(){
+    var loggedin = JSON.parse(localStorage.getItem("loggedIn"));
+    var navigationBarLeftButtons = document.getElementsByClassName("left_buttons")[0];
+    if(loggedin === false){
+        var profileButton = document.getElementById("profile_button");
+        var borrowBookButton = document.getElementById("borrow_book_button");
+        borrowBookButton.remove();
+        profileButton.remove();
+
+    }
+
+    try {
+        var accountType = JSON.parse(localStorage.getItem("userInfo")).account_type;
+    }
+    catch {
+        
+    };
+    
+    if(loggedin === true){
+        var loginButton = document.getElementById("login_button");
+        var signupButton = document.getElementById("signup_button");
+
+        loginButton.remove();
+        signupButton.remove();
+
+        var ulElement = document.createElement('ul');
+        ulElement.id = 'signout_button';
+        ulElement.className = 'nav_bar_button';
+
+        var aElement = document.createElement('a');
+        aElement.href = 'Signup.html';
+        aElement.textContent = 'Sign Out'; 
+
+        ulElement.appendChild(aElement);
+        navigationBarLeftButtons.appendChild(ulElement);
+    }
+
+    if(accountType == "User" || !loggedin){
+        var addBooksButton = document.getElementById("add_book_button");
+        addBooksButton.remove();
+    }
+})
+
 var searchInput;
 var filterSelect;
 //Search bar code
@@ -25,41 +68,3 @@ function extractSearchTextAndFilter() {
 }
 
 // Action bar Code
-var loggedin = JSON.parse(localStorage.getItem("loggedIn"));
-var accountType = JSON.parse(localStorage.getItem("userInfo")).account_type;
-
-document.addEventListener("DOMContentLoaded",function(){
-    var navigationBarLeftButtons = document.getElementsByClassName("left_buttons")[0];
-    if(loggedin === false){
-        var profileButton = document.getElementById("profile_button");
-        var borrowBookButton = document.getElementById("borrow_book_button");
-        borrowBookButton.remove();
-        profileButton.remove();
-
-    }
-    
-    if(loggedin === true){
-        var loginButton = document.getElementById("login_button");
-        var signupButton = document.getElementById("signup_button");
-
-        loginButton.remove();
-        signupButton.remove();
-
-        var ulElement = document.createElement('ul');
-        ulElement.id = 'signout_button';
-        ulElement.className = 'nav_bar_button';
-
-        var aElement = document.createElement('a');
-        aElement.href = 'Signup.html';
-        aElement.textContent = 'Sign Out'; 
-
-        ulElement.appendChild(aElement);
-        navigationBarLeftButtons.appendChild(ulElement);
-    }
-
-    if(accountType == "User" || !loggedin){
-        var addBooksButton = document.getElementById("add_book_button");
-        addBooksButton.remove();
-    }
-})
-
